f43b306fae89a6fde2aef9bcde2511dafd8519c7
bptato <nincsnevem662@gmail.com>
2023-01-05 03:10:30 +0100

    javascript: remove unused pointer case from getTypePtr
    I probably removed the last call of getTypePtr with a pointer long ago.

--- src/js/javascript.nim
+++ src/js/javascript.nim
@@ -727,7 +727,7 @@ proc toJS(ctx: JSContext, s: seq): JSValue =
   return a
 
 proc getTypePtr[T](x: T): pointer =
-  when T is RootRef or T is pointer:
+  when T is RootRef:
     # I'm so sorry.
     # (This dereferences the object's first member, m_type. Probably.)
     return cast[ptr pointer](x)[]
