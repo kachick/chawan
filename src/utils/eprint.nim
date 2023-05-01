e0a8b27068ac8fc61a243bae685c23a14976f619
bptato <nincsnevem662@gmail.com>
2023-05-01 09:48:56 +0200

    Add elog
    Like eprint, but writes to `a'

--- src/utils/eprint.nim
+++ src/utils/eprint.nim
@@ -20,3 +20,19 @@ func eprint*(s: varargs[string, `$`]) = {.cast(noSideEffect), cast(tags: []), ca
       o &= x
     o &= '\n'
     stderr.write(o)
+
+func elog*(s: varargs[string, `$`]) = {.cast(noSideEffect), cast(tags: []), cast(raises: []).}:
+  var f: File
+  if not open(f, "a", fmAppend):
+    return
+  var a = false
+  var o = ""
+  for x in s:
+    if not a:
+      a = true
+    else:
+      o &= ' '
+    o &= x
+  o &= '\n'
+  f.write(o)
+  close(f)
