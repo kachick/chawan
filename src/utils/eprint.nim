5d895934d4a89bb2efafc0de8e2009d880c7cfe0
bptato <nincsnevem662@gmail.com>
2023-06-23 00:32:50 +0200

    eprint: simplify

--- src/utils/eprint.nim
+++ src/utils/eprint.nim
@@ -1,38 +1,28 @@
 {.used.}
 
-func eprint*(s: varargs[string, `$`]) = {.cast(noSideEffect), cast(tags: []), cast(raises: []).}:
-  var a = false
-  var o = ""
-  when nimVm:
-    for x in s:
-      if not a:
-        a = true
-      else:
+func eprint*(s: varargs[string, `$`]) =
+  {.cast(noSideEffect), cast(tags: []), cast(raises: []).}:
+    var o = ""
+    for i in 0 ..< s.len:
+      if i != 0:
         o &= ' '
-      o &= x
-    echo o
-  else:
-    for x in s:
-      if not a:
-        a = true
-      else:
-        o &= ' '
-      o &= x
-    o &= '\n'
-    stderr.write(o)
-
-func elog*(s: varargs[string, `$`]) = {.cast(noSideEffect), cast(tags: []), cast(raises: []).}:
-  var f: File
-  if not open(f, "a", fmAppend):
-    return
-  var a = false
-  var o = ""
-  for x in s:
-    if not a:
-      a = true
+      o &= s[i]
+    when nimVm:
+      echo o
     else:
-      o &= ' '
-    o &= x
-  o &= '\n'
-  f.write(o)
-  close(f)
+      o &= '\n'
+      stderr.write(o)
+
+func elog*(s: varargs[string, `$`]) =
+  {.cast(noSideEffect), cast(tags: []), cast(raises: []).}:
+    var f: File
+    if not open(f, "a", fmAppend):
+      return
+    var o = ""
+    for i in 0 ..< s.len:
+      if i != 0:
+        o &= ' '
+      o &= s[i]
+    o &= '\n'
+    f.write(o)
+    close(f)
