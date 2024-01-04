4c81d51a304fea9d9e902ad8b5a28d1d3b9a9a03
bptato <nincsnevem662@gmail.com>
2024-01-04 18:59:08 +0100

    Merge data/idna with types/url
    No need to have a separate directory for just 3 functions.

--- src/types/url.nim
+++ src/types/url.nim
@@ -7,13 +7,15 @@ import std/tables
 import std/unicode
 
 import bindings/libunicode
-import data/idna
 import js/error
 import js/javascript
 import lib/punycode
 import types/blob
 import utils/luwrap
+import utils/map
 import utils/twtstr
+
+include res/map/idna_gen
 
 type
   URLState = enum
@@ -255,6 +257,52 @@ func cmpRange(x: u32pair, y: uint32): int =
   elif x.b < y:
     return -1
   return 0
+
+type
+  IDNATableStatus = enum
+    IDNA_VALID, IDNA_IGNORED, IDNA_MAPPED, IDNA_DEVIATION, IDNA_DISALLOWED
+
+func getIdnaTableStatus(r: Rune): IDNATableStatus =
+  let i = uint32(r)
+  if i <= high(uint16):
+    let u = uint16(i)
+    if u in IgnoredLow:
+      return IDNA_IGNORED
+    if u in DisallowedLow:
+      return IDNA_DISALLOWED
+    for item in Deviation:
+      if item[0] == u:
+        return IDNA_DEVIATION
+    if DisallowedRangesLow.isInRange(u):
+      return IDNA_DISALLOWED
+    if MappedMapLow.isInMap(u):
+      return IDNA_MAPPED
+  else:
+    if i in IgnoredHigh:
+      return IDNA_IGNORED
+    if i in DisallowedHigh:
+      return IDNA_DISALLOWED
+    if DisallowedRangesHigh.isInRange(i):
+      return IDNA_DISALLOWED
+    if MappedMapHigh.isInMap(uint32(i)):
+      return IDNA_MAPPED
+  return IDNA_VALID
+
+func getIdnaMapped(r: Rune): string =
+  let i = uint32(r)
+  if i <= high(uint16):
+    let u = uint16(i)
+    let n = MappedMapLow.searchInMap(u)
+    if n != -1:
+      return $MappedMapLow[n].mapped
+  let n = MappedMapHigh.searchInMap(i)
+  return $MappedMapHigh[n].mapped
+
+func getDeviationMapped(r: Rune): string =
+  for item in Deviation:
+    if item[0] == uint16(r):
+      return $item[1]
+  return ""
 
 func processIdna(str: string, checkhyphens, checkbidi, checkjoiners,
     transitionalprocessing: bool): Option[string] =
