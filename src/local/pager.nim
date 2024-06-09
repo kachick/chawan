48be38b8d04b9ca0c6796e259d762e4533d9a458
bptato <nincsnevem662@gmail.com>
2024-06-09 03:52:11 +0200

    pager: actually fix broken logic
    ok we block if:
    * method is not GET
    * scheme is different
    * it is not the case that both the current and the target schemes are
      http/s

--- src/local/pager.nim
+++ src/local/pager.nim
@@ -1863,8 +1863,8 @@ proc handleEvent0(pager: Pager; container: Container; event: ContainerEvent):
     let url = event.request.url
     let sameScheme = container.url.scheme == url.scheme
     if event.request.httpMethod != hmGet and not sameScheme and
-        container.url.scheme notin ["http", "https"] and
-        url.scheme notin ["http", "https"]:
+        not (container.url.scheme in ["http", "https"] and
+          url.scheme in ["http", "https"]):
       pager.alert("Blocked cross-scheme POST: " & $url)
       return
     #TODO this is horrible UX, async actions shouldn't block input
