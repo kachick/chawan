22625453ef3275adab3a47990c242dc92397b02b
bptato <nincsnevem662@gmail.com>
2024-08-01 21:10:39 +0200

    Update Chagashi

--- src/version.nim
+++ src/version.nim
@@ -27,6 +27,6 @@ tryImport chame/version, "chame"
 tryImport monoucha/version, "monoucha"
 
 static:
-  checkVersion("chagashi", 0, 5, 3)
+  checkVersion("chagashi", 0, 5, 4)
   checkVersion("chame", 1, 0, 1)
   checkVersion("monoucha", 0, 2, 3)
